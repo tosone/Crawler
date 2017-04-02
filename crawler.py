f8bfe30c103350d13a19be5c06e4e3df3ac32354
Tosone <i@tosiney.com>
2017-04-02 16:17:35 +0800

    change crawler
    Signed-off-by: Tosone <i@tosiney.com>

--- crawler.py
+++ crawler.py
@@ -1,7 +1,28 @@
 from os import listdir, path
+import threading
+import hashlib
+import time
+import os
 
 if __name__ == '__main__':
+    thread_list = {}
     for filename in listdir('.'):
-        if filename[0:2] == '__' and path.splitext(filename)[1] == '.py':
+        name = path.splitext(filename)[0]
+        ext = path.splitext(filename)[1]
+        if name[0:2] == '__' and ext == '.py':
+            pure_name = name[2:]
             run = __import__(path.splitext(filename)[0])
-            run.Run()
+            t = threading.Thread(target=run.Run)
+            thread_list[pure_name] = hashlib.md5(open(filename, 'rb').read()).hexdigest()
+            t.setDaemon(True)
+            t.setName(path.splitext(filename)[0])
+            t.start()
+    while True:
+        for filename in listdir('.'):
+            name = path.splitext(filename)[0]
+            ext = path.splitext(filename)[1]
+            if filename[0:2] == '__' and name == '.py':
+                pure_name = name[2:]
+                if thread_list.get(pure_name) != hashlib.md5(open(filename, 'rb').read()).hexdigest():
+                    os.exit(0)
+        time.sleep(20)
